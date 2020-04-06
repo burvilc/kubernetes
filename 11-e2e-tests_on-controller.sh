06fd9e178e07b0296981cc85562046d3cf576596
burvil <burvil@burvil.org>
2020-04-05 19:28:17 -0700

    Changed from kubetest to sonobuoy for E2E testing.  Kubetest taking too long, and there doesn't seem to be an easy way to break down the tests into smaller chunks.  It seems to be running into an IO bottleneck, where limits are reached for the EBS volume.

--- 11-e2e-tests_on-controller.sh
+++ 11-e2e-tests_on-controller.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-GO_TARBALL="go1.14.1.linux-amd64.tar.gz"
-wget https://dl.google.com/go/${GO_TARBALL}
-ls -lh $GO_TARBALL	
-
 sudo tar -C /usr/local -xzvf $GO_TARBALL 
 export GOPATH="${HOME}/go"
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
-go get -v -u k8s.io/test-infra/kubetest
-which kubetest
-sudo find $HOME -name kubetest
+EXE=sonobuoy
 
-kubetest --verbose --extract=v1.18.0
+go get -v -u github.com/vmware-tanzu/${EXE}
+ 
+which ${EXE} 
+sudo find $HOME -name ${EXE} 
 
-cd kubernetes
+${EXE} run --mode=certified-conformance --wait
+RESULTS=$(${EXE} retrieve)
 
-export KUBE_MASTER_IP_ADDR=`ip addr | egrep -A 3 ens5 | grep inet | grep -v inet6 | awk {'print $2'}  | sed 's/\/24//g'`
-export KUBE_MASTER_IP="${KUBE_MASTER_IP_ADDR}:6443"
-export KUBE_MASTER="controller-0"
-
-kubetest --verbose --test --provider=aws --test_args="--ginkgo.focus=\[Conformance\]" | tee 12-e2e-test-results.log
+${EXE} e2e $RESULTS 
